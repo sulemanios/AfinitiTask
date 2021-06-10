4da824deec0a9f4527e69eb3be94c49e7818d3c8
sulemanios <m_suleman86@yahoo.com>
2021-06-10 18:36:39 +0500

    Initial Commit

---
+++ AfinitiTaskTests/AfinitiTaskTests.swift
@@ -0,0 +1,34 @@
+//
+//  AfinitiTaskTests.swift
+//  AfinitiTaskTests
+//
+//  Created by Muhammad Suleman on 6/10/21.
+//  Copyright © 2021 Muhammad Suleman. All rights reserved.
+//
+
+import XCTest
+@testable import AfinitiTask
+
+class AfinitiTaskTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
